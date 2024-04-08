4b5346f2bb372de0e787a8e3a0e46b2ddbb17d6e
Admin <mishavolgodonsk@gmail.com>
2024-04-08 23:41:10 +0300

    5. PsqlStore [#285209]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -15,6 +15,25 @@ public class Post {
 
     private LocalDateTime created;
 
+    public Post() {
+
+    }
+
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
     public int getId() {
         return id;
     }
---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,120 @@
+package ru.job4j.grabber;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store {
+
+    private Connection connection;
+
+    public PsqlStore(Properties config) {
+        try {
+            Class.forName(config.getProperty("driver-class-name"));
+            connection = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement statement =
+                     connection.prepareStatement("INSERT INTO post(name, text, link, created) "
+                             + "VALUES (?, ?, ?, ?) ON CONFLICT (link) DO NOTHING")) {
+            statement.setString(1, post.getTitle());
+            statement.setString(2, post.getDescription());
+            statement.setString(3, post.getLink());
+            statement.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Post createPost(ResultSet resultSet) throws SQLException {
+        return new Post(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("link"),
+                resultSet.getString("text"),
+                resultSet.getTimestamp("created").toLocalDateTime()
+        );
+    }
+
+    private void addPosts(PreparedStatement statement, List<Post> posts) {
+        try (ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                posts.add(createPost(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> posts = new ArrayList<>();
+        try (PreparedStatement statement =
+                     connection.prepareStatement("SELECT * FROM post")) {
+            addPosts(statement, posts);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = null;
+        try (PreparedStatement statement =
+                     connection.prepareStatement(String.format(
+                             "SELECT * FROM post WHERE id = %d", id))) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    post = createPost(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        try (InputStream input = PsqlStore.class.getClassLoader()
+                .getResourceAsStream("rabbit.properties")) {
+            config.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (PsqlStore psql = new PsqlStore(config)) {
+            psql.save(new Post("post1", "111", "desc", LocalDateTime.now()));
+            psql.save(new Post("post2", "112", "desc", LocalDateTime.now()));
+            psql.save(new Post("post3", "113", "desc", LocalDateTime.now()));
+            psql.save(new Post("post4", "111", "desc", LocalDateTime.now()));
+            List<Post> postList = psql.getAll();
+            System.out.println(postList);
+            Post post = psql.findById(2);
+            System.out.println(post);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
